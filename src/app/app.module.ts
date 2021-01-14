9332bd65b662001245df5e9cc1bdf4f02bddd9de
awasevalentine <awasevalentine@gmail.com>
2021-01-14 11:18:48 +0100

    initialise the progress bar spinner for the sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { DashboardComponent } from './shared/dashboard/dashboard.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,28 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     AngularMaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    NgCircleProgressModule.forRoot({
+      "backgroundOpacity": 1,
+      "backgroundStrokeWidth": 4,
+      "backgroundPadding": 0,
+      "title":"",
+      "radius": 39,
+      "space": -2,
+      "toFixed": 0,
+      "maxPercent": 100,
+      "outerStrokeWidth": 3,
+      "outerStrokeColor": "#18e756",
+      "innerStrokeColor": "#1b34ee",
+      "innerStrokeWidth": 3,
+     
+      "subtitleColor": "#271124",
+      "subtitleFontSize": "19",
+      "imageHeight": 229,
+      "imageWidth": 104,
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showTitle": false,
+      "showUnits": true})
   ],
   providers: [],
   bootstrap: [AppComponent]
